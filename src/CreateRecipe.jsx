47cf9c0731f2430177ea2cf04efbec1d8741c1a4
Gary Bermudez <garybm.17@gmail.com>
2019-05-03 11:31:44 -0700

    start adding components to the project directory

---
+++ src/CreateRecipe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+class CreateRecipe extends React.Component {
+  render() {
+
+    return (
+
+    )
+  }
+}
+
+export default CreateRecipe;
---
+++ src/Error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Error404() {
+
+  return(
+
+  )
+
+}
+
+export default Error404;
