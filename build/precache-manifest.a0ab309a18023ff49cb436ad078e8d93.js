b28447144cf1f67ffa4a46f1ea2e5f04355612d6
Gary Bermudez <garybm.17@gmail.com>
2019-05-03 15:39:06 -0700

    created wireframes for the log in, sign in, dashboard, new recipe and recipe detail

---
+++ build/precache-manifest.a0ab309a18023ff49cb436ad078e8d93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87df89d83d2ce915e36209b43f0d82e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17152485b4f7e0bfda9d",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "15e1fc148b4b338092b4",
+    "url": "/static/js/2.1b22ae25.chunk.js"
+  },
+  {
+    "revision": "17152485b4f7e0bfda9d",
+    "url": "/static/js/main.e3bb35ac.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
