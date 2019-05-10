d5e4f66414cc666e103f37e5b13bda281424a367
Gary Bermudez <garybm.17@gmail.com>
2019-05-10 08:19:57 -0700

    remove media query from the navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,107 +12,99 @@ class Navigation extends React.Component {
       on: !this.state.on
     })
   }
-  render() {
 
+  render() {
   return (
     <div>
       <style jsx>{`
-          * {
-            margin: 0;
-            padding: 0;
+      * {
+        margin: 0;
+        padding: 0;
 
-          }
+      }
+      .menuLogo {
+        height: 10vh;
+        width: 100%;
+        display: flex;
+        // border: 1px red solid;
+      }
+      .hamburger {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        width: 50%;
+        justify-content: center;
+        align-items: flex-end;
+        cursor: pointer;
+        transform: translate( -10%);
+        // border: 1px red solid;
+      }
 
-          .hamburger {
-            display: flex;
-            flex-direction: column;
+      .imageContainer {
+        height: 100%;
+        width: 50%;
+        cursor: pointer;
+        // border: 1px red solid;
+      }
 
-            height: 10vh;
-            justify-content: center;
-            align-items: flex-end;
-            cursor: pointer;
-            transform: translate(-2%, -10%);
-          }
 
-          img {
-            width: 150px;
-            height: 150px;
-          }
-          .nav-links {
-            display: flex;
-            list-style: none;
-            width:50%;
-            height: 100%;
-            justify-content: space-around;
-            margin-left: auto;
-            align-items: center;
-          }
+      img {
+        width: 150px;
+        height: 150px;
+        // border: 1px red solid;
+        transform: translate( 10%, -25%);
 
-          .nav-links li a {
-            color: white;
-            text-decoration: none;
-          }
+      }
+      .nav-links {
+        display: flex;
+        list-style: none;
+        width:50%;
+        height: 100%;
+        justify-content: space-around;
+        margin-left: auto;
+        align-items: center;
+      }
 
-          .line {
-            width: 30px;
-            height: 3px;
-            background: #3D5E68;
-            margin: 5px;
-          }
-          nav {
-            position: relative;
-          }
+      .nav-links li a {
+        color: white;
+        text-decoration: none;
+      }
 
-          .nav-links {
-            position: fixed;
-            background-color: #f2c743;
-            height: 90vh;
-            width: 100%;
-            flex-direction: column;
-          }
+      .line {
+        width: 30px;
+        height: 3px;
+        background: #3D5E68;
+        margin: 5px;
+      }
+      nav {
+        position: relative;
+      }
 
-          @media screen and (max-width: 768px){
-
-            .line {
-              width: 30px;
-              height: 3px;
-              background: #3D5E68;
-              margin: 5px;
-            }
-
-            nav {
-              position: relative;
-            }
-
-            .hamburger {
-              position: absolute;
-              cursor: pointer;
-              right: 5%;
-              top: 60%;
-              transform: translate(-5%, -20%);
-              z-index: 2;
-            }
-
-            .nav-links {
-              position: fixed;
-              height: 100vh;
-              width: 100%;
-              flex-direction: column;
-            }
-          }
-              `}</style>
+      .nav-links {
+        position: fixed;
+        background-color: #f2c743;
+        height: 90vh;
+        width: 100%;
+        flex-direction: column;
+      }
+    `}</style>
       <nav>
-        <div className="hamburger" onClick={this.toggle}>
-          <div className="line"> </div>
-          <div className="line"> </div>
-          <div className="line"> </div>
+        <div className="menuLogo">
+          <div className="imageContainer">
+            <a href="#"><img src={logo} alt="website-logo"></img></a>
+          </div>
+          <div className="hamburger" onClick={this.toggle}>
+            <div className="line"> </div>
+            <div className="line"> </div>
+            <div className="line"> </div>
+          </div>
         </div>
         {this.state.on && (
           <ul className="nav-links">
-            <img src={logo} alt="website-logo"></img>
-            <li><a href="#">test</a></li>
-            <li><a href="#">test</a></li>
-            <li><a href="#">test</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Log In</a></li>
+            <li><a href="#">Log Out</a></li>
+            <li><a href="#">Add A Recipe</a></li>
           </ul>
         )}
       </nav>
