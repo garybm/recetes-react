5366bd3e548e42a19a8c418cd4bd159c858aae16
Gary Bermudez <garybm.17@gmail.com>
2019-05-20 14:24:04 +0200

    create the recipe details component and add a const to extract the id of each recipe users will load to eventually use it with the database

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import  { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navigation from './Navigation'
 import Dashboard from './Dashboard'
+import RecipeDetails from './RecipeDetails'
 
 
 class App extends React.Component {
@@ -11,7 +12,8 @@ class App extends React.Component {
       <div className="App">
           <Navigation />
           <Switch>
-            <Route path='/' component={Dashboard}/> 
+            <Route exact path='/' component={Dashboard}/>
+            <Route path='/recipe/:id' component={RecipeDetails}/>
           </Switch>
       </div>
     </BrowserRouter>
--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-function RecipeDetails () {
+const RecipeDetails = (props) => {
+  console.log(props);
+  const id = props.match.params.id;
   return (
-    <div>
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Recipe title - { id }</span>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et labore quaerat quibusdam vel saepe, ab voluptate accusantium culpa nemo fuga earum? Soluta amet nobis officia sed neque fuga aperiam quia?</p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by Gary</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
     </div>
   )
 }
+
   export default RecipeDetails;
